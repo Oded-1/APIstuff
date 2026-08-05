7641f42fb1f395380eefc73f780f936876f9536a
oded <oded.tzoreff@gmail.com>
2026-08-05 16:09:41 +0300

    primaryCalnderWorks

--- geminiAPItest.py
+++ geminiAPItest.py
@@ -11,9 +11,89 @@ from retry_requests import retry
 
 import yfinance as yf
 
-googleStock = yf.Ticker("GOOG")
-print(googleStock.history(period='3d'))
+import datetime
+import os.path
 
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+
+
+def calender():
+  """Shows basic usage of the Google Calendar API.
+  Prints the start and name of the next 10 events on the user's calendar.
+  """
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+  try:
+    service = build("calendar", "v3", credentials=creds)
+     
+    # Call the Calendar API
+    today = datetime.datetime.now(tz=datetime.timezone.utc).replace(hour = 0, minute = 0, second =0)
+    xHr = 48
+    afterXhr = today + datetime.timedelta(hours=xHr)
+
+    today = today.isoformat()
+    afterXhr = afterXhr.isoformat()
+
+    print(today, afterXhr)
+
+    print("Getting the upcoming events for the next ",xHr," hrs")
+    events_result = (
+        service.events()
+        .list(
+            calendarId="primary",
+            timeMin=today,
+            timeMax=afterXhr,
+            singleEvents=True,
+            orderBy="startTime",
+        )
+        .execute()
+    )
+    events = events_result.get("items", [])
+
+    if not events:
+      print("No upcoming events found.")
+      return
+
+    # Prints the start and name of the next 10 events
+    for event in events:
+      start = event["start"].get("dateTime", event["start"].get("date"))
+      print(start, event["summary"])
+
+  except HttpError as error:
+    print(f"An error occurred: {error}")
+
+
+
+calender()
+
+
+def stocks():
+    googleStock = yf.Ticker("GOOG")
+    return (googleStock.history(period='3d', interval='30m', rounding=True))
 
 def weather():
     # Setup the Open-Meteo API client with cache and retry on error
